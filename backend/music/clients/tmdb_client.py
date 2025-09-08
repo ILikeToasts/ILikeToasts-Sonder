4b7ba6a4fbc6696933a50507204ea19d67cb586c
ILikeToasts <jerome.turgeon0414@gmail.com>
2025-09-08 11:58:38 -0400

    small bugfix to update lists properly

--- backend/music/clients/tmdb_client.py
+++ backend/music/clients/tmdb_client.py
@@ -166,7 +166,7 @@ class TMDbClient:
 
         items = self.fetch_list_items(list_id)
 
-        if category == "Anime" or category == "TVShow":
+        if category == "anime" or category == "tv":
             media_type = "tv"
         else:
             media_type = "movie"
